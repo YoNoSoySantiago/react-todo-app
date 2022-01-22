c59be11da8aaf02c4a21b6d1249dc12fa7a93340
YoNoSoySantiago <santiago.hurtado1@hormail.com>
2022-01-22 10:41:49 -0500

    pasaporte terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import useGetTasks from "./hooks/useGetTasks";
 import AppContext from "./store/AppContext";
-import SearchBar from "./components/SearchBar/SearchBar";
+import TaskForm from "./components/TaskForm/TaskForm";
+import TasksList from "./components/TasksList/TasksList";
+import SortItems from "./components/SortItems/SortItems";
 import "./App.scss";
 
 const App = () => {
@@ -14,7 +16,9 @@ const App = () => {
 
   return (
     <div className="AppContainer">
-      <SearchBar />
+      <TaskForm />
+      <SortItems />
+      <TasksList />
     </div>
   );
 };
--- src/store/AppContext.jsx
+++ src/store/AppContext.jsx
@@ -1,11 +1,71 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
+import { v1 as uuid } from "uuid";
 
 const AppContext = createContext();
 
 export const AppContextWrapper = ({ children }) => {
   const [tasks, setTasks] = useState([]);
+  const [sortType, setSortType] = useState("asc");
 
-  const state = { tasks, setTasks };
+  useEffect(() => {
+    const newTasks = sortTasks(tasks);
+    setTasks(newTasks);
+    //eslint-disable-next-line
+  }, [sortType]);
+
+  const addTask = (taskName) => {
+    const newTask = {
+      userId: 1,
+      id: uuid(),
+      title: taskName,
+      completed: false,
+    };
+
+    const newTasks = [...tasks, newTask];
+
+    const taskWithOrder = sortTasks(newTasks);
+
+    setTasks(taskWithOrder);
+  };
+
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
+
+  const editTaskTitle = (taskId, newTitle) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          title: newTitle,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const sortTasks = (tasksToOrder) => {
+    const newTasks = tasksToOrder.map((task) => task);
+    if (sortType === "asc") {
+      newTasks.sort((a, b) => (a.title > b.title ? 1 : -1));
+    } else {
+      newTasks.sort((a, b) => (a.title < b.title ? 1 : -1));
+    }
+
+    return newTasks;
+  };
+
+  const state = {
+    tasks,
+    setTasks,
+    addTask,
+    deleteTask,
+    editTaskTitle,
+    sortType,
+    setSortType,
+  };
 
   return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
 };
